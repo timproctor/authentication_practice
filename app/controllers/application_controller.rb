bfa8de6b8f70c59f6477bcf1d8b7885718e98633
timproctor <timandcamiproctor@gmail.com>
2014-08-11 12:01:08 -0600

    Working on getting authorization tests to pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  filter_parameter_logging :password
   protect_from_forgery with: :exception
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,33 @@
 class User < ActiveRecord::Base
-	validates :login, presence: true
-	has_secure_password
+	attr_accessor :new_password, :new_password_confirmation
+	attr_reader :username, :password
+
+	validates_confirmation_of :new_password, :if=>:password_changed?
+
+	before_save :hash_new_password, :if=>:password_changed?
+
+	def self.create(data)
+		login = data[:username]
+		salt = BCrypt::Engine.generate_salt
+		digest = BCrypt::Engine.hash_secret(data[:password])
+	end
+
+	def password_changed?
+		!@new_password.blank?
+	end
+
+	def self.authenticate(login, password)
+		if user = find_by_login(login)
+			if BCrypt::Password.new(user.hashed_password).is_password? password
+				return user
+			end
+		end
+		return nil
+  end
+
+private
+	def hash_new_password
+		self.hashed_password = BCrypt::Password.create(@new_password)
+	end
+
 end
